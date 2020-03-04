08b08d9f1c38a3f8b3584a1d39b3778dacbfa149
YYiiEt <60560292+YYiiEt@users.noreply.github.com>
2020-03-04 20:35:12 +0800

    Update before_build.sh

--- before_build.sh
+++ before_build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd /home/runner/openwrt
 
+# Change timezone
+sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
 # Enter your commands here, e.g.
 # echo "Start build!"
 make defconfig
